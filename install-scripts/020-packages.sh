7d53915ea9099792416002713bbab4754647d8e3
Nathan Henrie <nate@n8henrie.com>
2018-02-23 13:49:42 -0700

    Use etckeeper

--- install-scripts/020-packages.sh
+++ install-scripts/020-packages.sh
@@ -5,3 +5,9 @@ rm -rf /var/cache/pacman/pkg
 ln -s /mnt/var/cache/pacman/pkg /var/cache/pacman/pkg
 
 pacstrap -c /mnt base base-devel archiso $(cat ~/packages/packages.both)
+cat <<-END | arch-chroot /mnt bash
+set -e
+cd /etc
+etckeeper init
+etckeeper commit -m "Initial commit"
+END
